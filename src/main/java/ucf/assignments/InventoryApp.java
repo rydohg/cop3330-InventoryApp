e1710d533f0e19d83033a6b139850a7662516d2a
rydohg <rdoh62001@gmail.com>
2021-07-24 12:38:46 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/InventoryApp.java
@@ -0,0 +1,29 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Ryan Doherty
+ */
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class InventoryApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/scene.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("Cool Inventory App Name");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
